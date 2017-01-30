1eaf65c6e92dcfd0105ee59bbd3acae88fefa05b
tomeehan <tom.meehan@me.com>
2017-01-30 18:49:02 +0000

    installed youtube-api-search

--- src/components/video_list.js
+++
@@ -1,1 +0,0 @@
-video_list.js
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDvirpGh6mOhj6sWSd3H-I-gqwW_dJ4m5w';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+	console.log(data);
+});
 
 const App  = () => {
-	return <div>hi!</div>;	
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
-
 
 ReactDOM.render(<App />, document.querySelector('.container'));
