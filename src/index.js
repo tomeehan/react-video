16da8b2bad2793fef2833feaad5eef164dbdac1e
tomeehan <tom.meehan@me.com>
2017-01-30 15:34:10 +0000

    init commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App  = () => {
+	return <div>hi!</div>;	
+}
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
