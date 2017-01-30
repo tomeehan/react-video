b165e7dbddadcb106a2e65ab69ac7c0c652f3f93
tomeehan <tom.meehan@me.com>
2017-01-30 20:15:17 +0000

    video detail

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -7,7 +7,7 @@ const VideoDetail = ({video}) => {
   return (
     <div className="video-detail col-md-6">
       <div className="embed-responsive embed-responsive-16by9">
-        <iframe className="embed-responsive-item"></iframe>
+        <iframe className="embed-responsive-item" src={url} ></iframe>
       </div>
 
       <div className="details">
