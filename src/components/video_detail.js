a8f2fcb208548ce8d85baa3545488f81c3ea6472
tomeehan <tom.meehan@me.com>
2017-01-30 20:14:14 +0000

    video list and video list components

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,1 +1,22 @@
-video_detail.js
+import React from 'react';
+
+const VideoDetail = ({video}) => {
+
+  const url = `https://www.youtube.com/embed${videoId}`;
+
+  return (
+    <div className="video-detail col-md-6">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item"></iframe>
+      </div>
+
+      <div className="details">
+    	<div>{ video.snippet.title} </div>
+    	<div>{ video.snippet.description} </div>
+      </div>
+    </div>
+)
+
+}
+
+export default VideoDetail;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDvirpGh6mOhj6sWSd3H-I-gqwW_dJ4m5w';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-	console.log(data);
-});
+class App  extends Component {
+  
+  constructor(props) {
+  	super(props);
 
-const App  = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+  	this.state = { videos: [] }; 
+
+  	YTSearch({key: API_KEY, term: 'Teahupoo'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
