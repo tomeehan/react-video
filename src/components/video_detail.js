99e288bd9beb8fefdb778052f70a94db73fb0a8f
tomeehan <tom.meehan@me.com>
2017-01-30 22:50:47 +0000

    styling; re-factored API search in seperate function

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -10,14 +10,14 @@ const VideoDetail = ({video}) => {
   const url = `https://www.youtube.com/embed/${videoId}`;
 
   return (
-    <div className="video-detail col-md-6">
+    <div className="video-detail col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
         <iframe className="embed-responsive-item" src={url} ></iframe>
       </div>
 
       <div className="details">
-    	<div>{ video.snippet.title} </div>
-    	<div>{ video.snippet.description} </div>
+    	<div className="video-title">{ video.snippet.title} </div>
+    	<div className="video-desc">{ video.snippet.description} </div>
       </div>
     </div>
 )
--- src/index.js
+++ src/index.js
@@ -17,10 +17,14 @@ class App  extends Component {
   		selectedVideo: null 
   	}; 
 
-  	YTSearch({key: API_KEY, term: 'Teahupoo'}, (videos) => {
+    this.videoSearch('Teahupoo') 
+  }
+
+  videoSearch(term) {
+      YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({ 
-      	videos: videos,
-      	selectedVideo: videos[0]
+        videos: videos,
+        selectedVideo: videos[0]
       });
     });
   }
@@ -28,7 +32,7 @@ class App  extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList 
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
